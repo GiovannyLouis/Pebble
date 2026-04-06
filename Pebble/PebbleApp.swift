b0334287c47b65e6c4eda734b9507ec45b906028
Giovanny Louis <louis.anth10@gmail.com>
2026-04-06 14:34:19 +0700

    Initial Commit

---
+++ Pebble/PebbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PebbleApp.swift
+//  Pebble
+//
+//  Created by Vrz on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PebbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
