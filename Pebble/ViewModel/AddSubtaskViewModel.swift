8c516fce85671519ef8dc8c821515a052c59de2a
Deny Wahyudi Asaloei <115266792+denywa@users.noreply.github.com>
2026-04-10 09:45:33 +0700

    feat: add AddSubtaskView UI and AddSubtaskViewModel for state management

---
+++ Pebble/ViewModel/AddSubtaskViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  Pebble
+//
+//  Created by Deny Wahyudi Asaloei  on 09/04/26.
+//
+import SwiftUI
+
+@Observable
+class AddSubtaskViewModel {
+    var subtaskName = ""
+    
+    //Cek jika form sudah terisi atau belum
+    var isChanged: Bool{
+        subtaskName != ""
+    }
+    
+    //if isChange True show discard alert/action sheet
+    var isShowingDiscardAlert:Bool = false
+}
